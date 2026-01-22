c166bad2745fdfb9a216c76ee4c68a5743f0b83c
User <code4comf@gmail.com>
2026-01-22 07:07:26 -0800

    Added new variant of todo list vue (2)

--- Js/todo_list_vue_2/script.js
+++ Js/todo_list_vue_2/script.js
@@ -1,17 +1,56 @@
 Vue.createApp({
-    data() {
+    props: {
+        item: {
+            type: Object,
+            required: true
+        }
+    },
 
+    data() {
+        return {
+            items: [],
+            newTodoItemId: 1,
+            newTodoItemText: "",
+            isEditing: false
+        };
     },
 
     methods: {
+        addTodoItem() {
+            const newTodoItem = {
+                id: this.newTodoItemId,
+                text: this.newTodoItemText
+            };
 
+            this.newTodoItemId++;
+
+            this.items.push(newTodoItem);
+
+            this.newTodoItemText = "";
+        },
+
+        deleteTodoItem(index) {
+            this.items.splice(index, 1);
+        },
+
+        editTodoItem() {
+
+        },
+
+        saveTodoItem() {
+
+        },
+
+        canselEditing() {
+
+        }
     },
 
     template:`
       <h1>TODO List</h1>
-      <form @submit.prevent="//TODO method">
+      <form @submit.prevent="addTodoItem">
         <label>
-          <input class="form-control" type="text">
+          <input v-model="newTodoItemText" class="form-control" type="text">
         </label>
         <div>
           <button>Add</button>
@@ -19,7 +58,13 @@ Vue.createApp({
       </form>
       
       <ul class="list-unstyled">
-        
-      </ul>
-    `
+        <li v-for="(item, index) in items"
+            :key="item.id">
+          <div>
+            {{  item.text  }}
+            <button @click="editTodoItem">Edit</button>
+            <button @click="deleteTodoItem(index)">Delete</button>
+          </div>
+        </li>
+      </ul>`
 }).mount("#app");
