fe24509df06607b9a2957ac42a3c5c012e977c6f
userm <code4comf@gmail.com>
2026-04-02 14:33:50 +0700

    Edited new "ExpressTestTask"

--- ExpressTest/public/javascripts/script.js
+++ ExpressTest/public/javascripts/script.js
@@ -1,3 +1,5 @@
+/* global axios, Vue, bootstrap */
+
 function executeGet(url, data) {
     return axios.get(url, {
         params: data
@@ -37,12 +39,18 @@ Vue.createApp({
             term: "",
             name: "",
             phone: "",
-            service: new PhoneBookService()
+            service: new PhoneBookService(),
+            deleteConfirmDialog: null,
+            contactToDelete: null
         };
     },
 
     created() {
         this.loadContacts();
+    },
+
+    mounted() {
+        this.deleteConfirmDialog = new bootstrap.Modal(this.$refs.modal, {});
     },
 
     methods: {
@@ -65,14 +73,21 @@ Vue.createApp({
             }).catch(() => alert("Couldn't create contacts"));
         },
 
-        deleteContact(contact) {
-            this.service.deleteContact(contact.id).then(response => {
+        showDeleteContactConfirmModal(contact) {
+            this.contactToDelete = contact;
+            this.deleteConfirmDialog.show();
+        },
+
+        deleteContact() {
+            this.service.deleteContact(this.contactToDelete.id).then(response => {
                 if (!response.success) {
                     alert(response.message);
                     return;
                 }
 
+                this.deleteConfirmDialog.hide();
                 this.loadContacts();
+                this.contactToDelete = null;
             }).catch(() => alert("Couldn't delete contacts"));
         },
 
